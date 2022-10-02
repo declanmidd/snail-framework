7c6abbea5754cfce00e9131f3d584d26d351d8a5
Declan Middleton <declanredd1722@gmail.com>
2022-10-02 22:04:53 +1300

    🔨

---
+++ install.sh
@@ -0,0 +1,13 @@
+if [["$OSTYPE" === "linux" ]]; then
+    sudo apt-get install -y
+    sudo apt-get install git -y
+    sudo apt-get install nodejs -y
+    sudo apt-get install curl -y
+    sudo apt-get install nmap -y
+    sudo apt-get install python2 -y
+    sudo apt-get install python3 -y
+    sudo apt-get install pip3 -y
+    sudo apt-get install whatweb -y
+    sudo apt-get install netdiscover -y
+    sudo apt-get install hping3 -y
+fi
